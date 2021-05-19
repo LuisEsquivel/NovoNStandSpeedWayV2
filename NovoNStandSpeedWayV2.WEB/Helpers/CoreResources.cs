b39061436183c5609de449cb47da27fa24214493
Luis Esquivel <luisesquivel7f5@email.com>
2021-05-19 09:11:58 -0500

    URL BASE DEL API

--- NovoNStandSpeedWayV2.WEB/Helpers/CoreResources.cs
+++ NovoNStandSpeedWayV2.WEB/Helpers/CoreResources.cs
@@ -5,12 +5,13 @@ namespace NovoNStandSpeedWayV2.WEB.Helpers
     public static class CoreResources
     {
 
-        public static string UrlBase = "http://localhost:100/";
+        public static string UrlBase = "http://luisesquivel:333/";
         //public static string UrlBase = "https://localhost:44325/";
         
         public static string Prefix = "api";
         public static string UserController = "user";
         public static string ProductController  = "product";
-
+        public static string RolesController  = "roles";
+   
     }
 }
